7636390202e09525006a2f26bd2dd5359f854902
Dinesh <dineshkumar.m@changepond.com>
2020-01-09 20:18:37 +0530

    Testing

---
+++ src/main/java/Test/test_war/App.java
@@ -0,0 +1,13 @@
+package Test.test_war;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Testing Hello World!" );
+    }
+}
